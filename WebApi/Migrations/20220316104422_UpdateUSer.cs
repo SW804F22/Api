d565d39c87946056756ac1bde097e08c115c12e1
AdrianPlesner <adrian.plesner@gmail.com>
2022-03-16 11:51:32 +0100

    new migration

---
+++ WebApi/Migrations/20220316104422_UpdateUSer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    public partial class UpdateUSer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Age",
+                table: "Users",
+                newName: "Gender");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Gender",
+                table: "Users",
+                newName: "Age");
+        }
+    }
+}
